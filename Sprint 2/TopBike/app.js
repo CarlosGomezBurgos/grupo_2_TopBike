a4adc6646f08676d32a05f41b73040d96eb39bf2
CarlosGomezBurgos <gbremerasestampadas@gmail.com>
2022-01-07 21:37:55 -0300

    Vistas completas mobile

--- Sprint 2/TopBike/app.js
+++ Sprint 2/TopBike/app.js
@@ -1,7 +1,11 @@
 const express = require ('express');
+
 const app = express();
+
 const path = require('path');
-app.use(express.static('public'))
+
+app.use(express.static('public'));
+
 const port = 4000;
 app.listen(port, () => {
     console.log('El servidor esta funcionando en el puerto:',port)
