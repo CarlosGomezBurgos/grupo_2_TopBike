28145e4d9bdb51e9c9f4038327820f2a99e8f067
CarlosGomezBurgos <gbremerasestampadas@gmail.com>
2022-01-27 14:09:19 -0300

    cambios sprint 4 27-01-22

--- Sprint 2/TopBike/routes/product.js
+++ Sprint 2/TopBike/routes/product.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const multer = require ('multer');
 
 const productController  = require('../controllers/productController')
 
+/* Multer config */
+const fileStorageEngine = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null,'./public/img/products')
+    },
+    filename: (req, file, cb)=>{
+        cb(null,`${Date.now()}--${file.originalname}`)
+}})
 
-router.get('/detail', productController.detail);
+const upload = multer({storage: fileStorageEngine})
+
+
+
+router.get('/', productController.index);
+
+router.get('/detail/:id', productController.detail);
+
 router.get('/cart', productController.cart);
+
+/* create form */
+router.get('/create',productController.create);
+router.post('/', upload.single('image'),productController.store);
+
+/* edit form */
+router.get('/edit/:id', productController.edit); 
+router.patch('/edit/:id', productController.update); 
 
 module.exports = router;
 
