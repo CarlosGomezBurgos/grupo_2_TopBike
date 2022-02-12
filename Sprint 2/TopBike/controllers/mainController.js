3f1bf8860987434c1737473849be0862ff339d1a
CarlosGomezBurgos <gbremerasestampadas@gmail.com>
2022-02-12 19:14:11 -0300

    update gral 12-02-22

--- Sprint 2/TopBike/controllers/mainController.js
+++ Sprint 2/TopBike/controllers/mainController.js
@@ -7,7 +7,7 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 const mainController = {
     index: (req,res) => {
          res.render('index',{productos: products});
-    },
+    }
 }
           
 module.exports = mainController;
