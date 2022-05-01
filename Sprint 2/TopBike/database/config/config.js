82ae25a630fcca0d9d6cfd02c96a7f77d7ba2cc3
CarlosGomezBurgos <gbremerasestampadas@gmail.com>
2022-05-01 13:35:12 -0300

    update validations front-back + bd + api sprint8 01-05-22

--- Sprint 2/TopBike/database/config/config.js
+++ Sprint 2/TopBike/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "carlo$227567",
+    "password": "",
     "database": "topbike",
     "host": "127.0.0.1",
     "dialect": "mysql"
