120882c60446397d8b332283a3e10325602951a9
MarinaArrieta <arrietamarina12@gmail.com>
2022-05-10 18:56:56 -0300

    mas estilos

--- Sprint 2/TopBike/public/js/productCreateFormValidations.js
+++ Sprint 2/TopBike/public/js/productCreateFormValidations.js
@@ -1,43 +1,57 @@
  window.addEventListener('load',function(){
     document.querySelector('form').addEventListener('submit', function(e){
-        let errorsList = [];
+        //let errorsList = [];
+        let error_counter = 0
         
-        let name = document.querySelector('input#name').value
+        let name = document.querySelector('input#name').value;
+        let name_error_container = document.querySelectorAll('.errors')[0]
         if (name == "") {
-            errorsList.push('el campo de "Nombre del producto" --> está incompleto')
+            error_counter++;
+            name_error_container.innerHTML = 'el campo de NOMBRE DEL PRODUCTO está incompleto'
         } else {
             if(name.length < 2) {
-                errorsList.push('el campo "Nombre del producto" deberá tener al menos 2 caracteres')
-            }
+                error_counter++;
+                name_error_container.innerHTML = 'el campo NOMBRE DEL PRODUCTO deberá tener al menos 2 caracteres'
+            }else{name_error_container.innerHTML = ''}
         }
 
         let price = document.querySelector('input#price').value
+        let price_error_container = document.querySelectorAll('.errors')[1]
         if (price == "") {
-            errorsList.push('el campo de "Precio del producto" --> está incompleto')
-        }
+            error_counter++;
+            price_error_container.innerHTML = 'el campo de PRECIO DEL PRODUCTO está incompleto'
+        }else{price_error_container.innerHTM = ''}
 
         let discount = document.querySelector('input#discount').value
+        let discount_error_container = document.querySelectorAll('.errors')[2]
         if (discount == "") {
-            errorsList.push('el campo de "Descuento" --> está incompleto')
-        }
+            error_counter++;
+            discount_error_container.innerHTML = 'el campo de DESCUENTO está incompleto'
+        }else{discount_error_container.innerHTM = ''}
         
         let description = document.querySelector('textarea#description').value
+        let description_error_container = document.querySelectorAll('.errors')[3]
         if (description == "") {
-            errorsList.push('el campo de "Descripcion" --> está incompleto')
+            error_counter++;
+            description_error_container.innerHTML = 'el campo de DESCRIPCION está incompleto'
         } else {
             if(description.length < 50) {
-                errorsList.push('el campo "Descripcion" deberá tener al menos 20 caracteres')
-            }
+                error_counter++;
+                description_error_container.innerHTML = 'el campo DESCRIPCION deberá tener al menos 20 caracteres'
+            }else{description_error_containe.innerHTM = ''}
         }
         
         let fileInput = document.querySelector('input#file');
+        let fileInput_error_container = document.querySelectorAll('.errors')[4]
         let filePath = fileInput.value;
         let allowedExtensions = /(.jpg|.jpeg|.png)$/i;
         if (!filePath) {
-            errorsList.push('El campo "Imagen de perfil" incompleto')
+            error_counter++;
+            fileInput_error_container.innerHTML = 'El campo IMAGEN DE PERFIL incompleto'
         } else {
             if(!allowedExtensions.exec(filePath)){
-                errorsList.push('Ingresar un formato de archivo válido: .jpeg/.jpg/.png/');
+                error_counter++;
+                fileInput_error_container.innerHTML = 'Ingresar un formato de archivo válido: .jpeg/.jpg/.png/';
             }             
         }
 
